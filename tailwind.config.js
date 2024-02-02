92a8d9be2ee068da051d06a0f02a3dbf0b1538d3
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-02-02 18:56:13 +0100

    feat: create theme from predefined figma mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#000853',
+        secondary: '#898DA9',
+        border: {
+          default: '#CBB6E5',
+          active: '#761BE4',
+          error: '#ED4545',
+        },
+      },
+      backgroundColor: {
+        inactive: '#CBB6E5',
+        default: '#761BE4',
+        hover: '#6A19CD',
+        iconHover: '#ED4545',
+      },
+    },
   },
   plugins: [],
 };
