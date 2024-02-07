9ad796ca6292f314fdd457fa7e14d704b202bfeb
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-02-07 18:46:04 +0100

    fix: add offset to outline for radio input

--- src/components/labeled-radio-input.tsx
+++ src/components/labeled-radio-input.tsx
@@ -24,14 +24,14 @@ const LabeledRadioInput = ({ children, min, max }: { children: string; min: numb
           setCurrentValue(+value.target.value);
         }}
         className={
-          'relative mt-5 h-1 cursor-pointer appearance-none rounded-full accent before:absolute before:bottom-4 before:content-[attr(data-before)] after:absolute after:bottom-4 after:right-0 after:content-[attr(data-after)] focus-visible:outline-active dark:bg-inactive'
+          'relative mt-5 h-1 cursor-pointer appearance-none rounded-full accent outline-offset-2 before:absolute before:bottom-4 before:content-[attr(data-before)] after:absolute after:bottom-4 after:right-0 after:content-[attr(data-after)] focus-visible:outline-active dark:bg-inactive'
         }
         name={children}
       />
       <output
         style={{ left: `calc(${currentOffset}% + (${8 - currentOffset * 0.15}px))` }}
         className={
-          'text-active relative mt-3 grid h-[33px] w-0 place-items-center justify-center before:absolute before:z-[-1] before:content-numberBubble'
+          'relative mt-3 grid h-[33px] w-0 place-items-center justify-center text-active before:absolute before:z-[-1] before:content-numberBubble'
         }>
         {currentValue}
       </output>
