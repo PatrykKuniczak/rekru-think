a0a85cb1b36f21ec16071b5a77af9089455a1784
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2024-02-10 20:04:26 +0100

    Revert "feat/create ui for deskotp (#3)"
    This reverts commit e54f8e87b75592f8422972b22a4b46c4b6290e0e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-import LabeledInput from './components/labeled-input.tsx';
-import LabeledRadioInput from './components/labeled-radio-input.tsx';
-import LabeledDragAndDropInput from './components/labeled-drag-and-drop-input.tsx';
-import LabeledCalendar from './components/calendar/labeled-calendar.tsx';
-import LabeledTimePicker from './components/labeled-time-picker.tsx';
-import { useState } from 'react';
-
 function App() {
-  const [shouldOpenTimePicker, setShouldOpenTimePicker] = useState(false);
-
-  return (
-    <div className={'flex h-screen w-screen justify-center'}>
-      <form className={'flex h-screen w-[426px] flex-col gap-4 pt-[120px]'}>
-        <h1 className={'mb-2 text-l font-semibold'}> Personal Info </h1>
-        <LabeledInput>First Name</LabeledInput>
-        <LabeledInput>Last Name</LabeledInput>
-        <LabeledInput>Email Address</LabeledInput>
-        <LabeledRadioInput min={8} max={100}>
-          Age
-        </LabeledRadioInput>
-        <LabeledDragAndDropInput>Photo</LabeledDragAndDropInput>
-        <h1 className={'text-l font-semibold'}> Your workout </h1>
-        <div className={'flex gap-7'}>
-          <LabeledCalendar selectedYear={2023} setShouldOpenTimePicker={setShouldOpenTimePicker}>
-            Date
-          </LabeledCalendar>
-          {shouldOpenTimePicker && <LabeledTimePicker>Time</LabeledTimePicker>}
-        </div>
-        <button className={'rounded bg py-2.5 text-white hover:bg-hover'}>Send Application</button>
-      </form>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
