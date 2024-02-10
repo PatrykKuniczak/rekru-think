ba2bb5e1ddd87e3a832187d68fc114ba5d3f9bd3
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-02-10 17:37:00 +0100

    fix: add missing border radius to text inputs

--- src/components/labeled-input.tsx
+++ src/components/labeled-input.tsx
@@ -1,9 +1,9 @@
 const LabeledInput = ({ children }: { children: string }) => (
   <div className={'flex flex-col'}>
-    <label className={'text mb-2'} htmlFor={children}>
+    <label className={'mb-2'} htmlFor={children}>
       {children}
     </label>
-    <input className={'h-[48px] px-1 outline outline-1 focus-visible:outline-active'} name={children} />
+    <input className={'h-[48px] rounded px-1 outline outline-1 focus-visible:outline-active'} name={children} />
   </div>
 );
 
