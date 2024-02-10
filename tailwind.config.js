b9284811ea4e9f9c62df88c3b41e951ccbfb3dd4
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2024-02-10 20:04:41 +0100

    Revert "feat/create ui for deskotp (#3)" (#4)
    This reverts commit e54f8e87b75592f8422972b22a4b46c4b6290e0e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,27 @@ export default {
   content: ['./index.html', './src/**/*.{jsx,tsx}'],
   theme: {
     extend: {
-      outlineColor: {
-        DEFAULT: '#CBB6E5',
+      borderColor: {
+        default: '#CBB6E5',
         active: '#761BE4',
         error: '#ED4545',
       },
-      accentColor: {
-        DEFAULT: '#761BE4',
-      },
-      outlineWidth: {
-        DEFAULT: '1px',
-      },
       fontSize: {
-        sm: '14px',
-        DEFAULT: '16px',
+        sm: '16px',
+        md: '18px',
         l: '24px',
       },
       colors: {
-        DEFAULT: '#000853',
+        primary: '#000853',
         secondary: '#898DA9',
-        active: '#761BE4',
       },
       backgroundColor: {
-        DEFAULT: '#761BE4',
         inactive: '#CBB6E5',
+        default: '#761BE4',
         hover: '#6A19CD',
         iconHover: '#ED4545',
       },
-      content: {
-        numberBubble: 'url("./src/assets/number-bubble.svg")',
-      },
-      borderRadius: {
-        DEFAULT: '8px',
-      },
     },
   },
-  safelist: [{ pattern: /col-span-([12345])/ }],
   plugins: [],
 };
